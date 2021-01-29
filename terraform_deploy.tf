0d319e6aa49191924a553a118a3b74ef905ec19f
Chandrakala-KH <42860183+Chandrakala-KH@users.noreply.github.com>
2021-01-29 13:10:55 +1100

    Update terraform_deploy.tf

--- terraform_deploy.tf
+++ terraform_deploy.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-  region = "us-east-2"
-  access_key = "AKIAJQAYZJN6BPVB6PDA"
-  secret_key = "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"
-}
+
 
 resource "aws_instance" "webinstance" {
    ami           = "ami-03d64741867e7bb94"
