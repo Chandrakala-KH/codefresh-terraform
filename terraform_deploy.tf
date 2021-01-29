e5d2aceefbce12b1b8838e41df69ce8e010e4a49
Chandrakala-KH <42860183+Chandrakala-KH@users.noreply.github.com>
2021-01-29 12:23:00 +1100

    tf file

---
+++ terraform_deploy.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "webinstance" {
+   ami           = "ami-03d64741867e7bb94"
+   instance_type = "t2.micro"
+   key_name      = "${var.privatekey}"
+   security_groups = ["${aws_security_group.mysg.name}"]
+   tags = {
+   Name = "webserver"
+   }
+   }
+resource "aws_security_group" "mysg" {
+  vpc_id       = "vpc-a66edccd"
+  name         = "websg"
+  description  = "My VPC Security Group"
+  
+  # allow ingress of port 22
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  
+} 
+  
+  # allow egress of all ports
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
