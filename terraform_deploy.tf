20590dee2712def2e545773487d00bb1e64eb71d
Chandrakala-KH <42860183+Chandrakala-KH@users.noreply.github.com>
2021-02-10 11:03:42 +1100

    commit with new changes
    code

--- terraform_deploy.tf
+++ terraform_deploy.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "webinstance" {
    ami           = "ami-03d64741867e7bb94"
    instance_type = "t2.micro"
    tags = {
-   Name = "webserver"
+   Name = "webserver-codefresh"
    }
    }
